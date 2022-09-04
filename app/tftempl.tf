588ceb4594b94dc59da35b616dbaeb40a6de19ec
Serge <domainsdeals@gmail.com>
2022-09-04 12:13:32 +0300

    hw

--- app/tftempl.tf
+++ app/tftempl.tf
@@ -35,7 +35,7 @@ default = "sg-0e2711cc241cc7671"
 
 resource "aws_s3_bucket" "storage" {
   bucket = "s3_storage"
-  profile = "public-read-write"
+  acl = "public-read-write"
   tags = {
     Name        = "s3_storage"
   }
