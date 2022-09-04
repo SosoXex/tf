5c145b0c1d327912e0fb54f8aa24b9c90ada39e2
Serge <domainsdeals@gmail.com>
2022-09-04 11:56:44 +0300

    hw

--- app/tftempl.tf
+++ app/tftempl.tf
@@ -19,29 +19,47 @@ variable "ami" {
   default = "ami-0c9354388bb36c088"
 }
 variable "itype" {
-  type = list(string)
-  default = ["t2.micro"]
+  default = "t2.micro"
 }
 
-variable "rtags" {
-  default     = "l15instance"
-  }
 variable "kp"{
 default = "L14"
 }
+
 variable "cnt"{
 default = "1"
 }
 variable "ivpc"{
 default = "sg-0e2711cc241cc7671"
 }
-resource "aws_instance" "test"{
+
+resource "aws_s3_bucket" "STORAGE" {
+  bucket = "s3_storage"
+  acl    = "private"
+  tags = {
+    Name        = "S3 storage"
+  }
+
+resource "aws_instance" "BUILD"{
        count = var.cnt
        key_name = var.kp
        ami = var.ami
        instance_type = element(var.itype, count.index)
        tags = {
-         Name = "${var.rtags}-${count.index+1}"
+         Name = "BUILD"
+       }
+       security_groups = [var.ivpc]
+       subnet_id = var.snet
+       associate_public_ip_address = true
+}
+
+resource "aws_instance" "WEB"{
+       count = var.cnt
+       key_name = var.kp
+       ami = var.ami
+       instance_type = element(var.itype, count.index)
+       tags = {
+         Name = "WEB"
        }
        security_groups = [var.ivpc]
        subnet_id = var.snet
