a1558c893a5963af133b941f04f24466d55e0cfd
Serge <domainsdeals@gmail.com>
2022-09-04 11:20:14 +0300

    template

---
+++ tftempl.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "eu-central-1"
+}
+
+variable "snet"{
+  default = "subnet-081298183b5a18fe4"
+}
+variable "ami" {
+  default = "ami-0c9354388bb36c088"
+}
+variable "itype" {
+  default = "t2.micro"
+}
+
+}
+variable "rtags" {
+  default     = "l15instance"
+  }
+variable "kp"{
+default = "L14"
+}
+variable "cnt"{
+default = "1"
+}
+variable "ivpc"{
+default = "sg-0e2711cc241cc7671"
+}
+resource "aws_instance" "test"{
+       count = var.cnt
+       key_name = var.kp
+       ami = var.ami
+       instance_type = var.itype
+       tags = {
+         Name = "${var.rtags}-${count.index+1}"
+       }
+       security_groups = [var.ivpc]
+       subnet_id = var.snet
+       associate_public_ip_address = true
+}
